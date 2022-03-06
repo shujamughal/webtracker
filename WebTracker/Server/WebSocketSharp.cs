9937c5b810e1b76ceef5376986c11273f8a7a6c3
saimbintalib <49138179+saimbintalib@users.noreply.github.com>
2022-03-06 14:31:59 +0500

    Implemented Console Socket

---
+++ WebTracker/Server/WebSocketSharp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+namespace Server
+{
+    public class Index : WebSocketBehavior
+    {
+        protected override void OnOpen()
+        {
+            Console.WriteLine("Cleint Conntected!");
+        }
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Data Received: " + e.Data);
+        }
+        protected override void OnError(ErrorEventArgs e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Console.WriteLine("Client Disconnected!");
+        }
+    }
+    class WebSocketSharp
+    {
+        static void Main(string[] args)
+        {
+            WebSocketServer server = new WebSocketServer("ws://127.0.0.1:5000");
+            server.AddWebSocketService<Index>("/index");
+            server.Start();
+            Console.WriteLine("Server Started! Press Any key to end the server!");
+            Console.ReadKey();
+            server.Stop();
+            Console.WriteLine("Server Stopped!");
+        }
+    }
+}
